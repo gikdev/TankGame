979024f858724377186f0d7bc192ab9ef42939ec
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2023-12-22 11:03:01 +0330

    Fixed the way that game findes the winner!

--- src/logic/_Game.ts
+++ src/logic/_Game.ts
@@ -8,7 +8,7 @@ export default class Game {
   #gridSize: number = 4;
   #player = Player.red;
   #mode = GameMode.arrange;
-  #aliveTanks = { red: 0, blue: 0 };
+  #aliveTanks: any = { red: 0, blue: 0 };
   #winner: Player | null = null;
 
   constructor(parent: Element, gridSize: number = 4) {
@@ -17,6 +17,10 @@ export default class Game {
     this.generateGrid();
     this.renderGame();
   }
+
+  /**
+   * Generates the `state` object
+   */
   private generateGrid(): void {
     let letters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
     [`blue`, `red`].forEach((color) => {
@@ -25,6 +29,10 @@ export default class Game {
           this.#state[color][`${letters[i]}${j + 1}`] = {};
     });
   }
+
+  /**
+   * Renders the game and set the grids' event listeners
+   */
   private renderGame(): void {
     this.#parent.innerHTML = ``;
     this.renderGrid(`blue`, this.#state.blue);
@@ -34,30 +42,55 @@ export default class Game {
       grid.addEventListener(`click`, this.handleGridClick)
     );
   }
+
+  /**
+   * Handles the click event on the grids
+   * @param e {*} Event Object passed to it
+   */
   private handleGridClick = (e: any): void => {
-    const isTargetImage = e.target.tagName === "IMG";
+    const isTargetImage = e.target.tagName === `IMG`;
     const currentTarget = isTargetImage ? e.target.parentElement : e.target;
     const address = currentTarget.dataset.address;
     const color = currentTarget.parentElement.id;
+
+    console.log({
+      alive: this.#aliveTanks,
+      state: this.#state,
+      winner: this.#winner,
+      mode: this.#mode,
+    });
 
     if (this.#mode === GameMode.play) {
       const enemyColor = color === `blue` ? `red` : `blue`;
       const enemyCell = this.#state[enemyColor][address];
       const isEnemyCellATank = enemyCell instanceof Tank;
       if (isEnemyCellATank) enemyCell.destroy();
-      if (enemyColor === Player.red) this.#aliveTanks.red--;
-      if (enemyColor === Player.blue) this.#aliveTanks.blue--;
-      this.changePlayer();
+      const alives: any = { blue: 0, red: 0 };
+      [`blue`, `red`].forEach((color) => {
+        for (const cellAddr in this.#state[color]) {
+          const cell = this.#state[color][cellAddr];
+          if (cell instanceof Tank && !cell.isDead) alives[color]++;
+          this.#aliveTanks[color] = alives[color]
+        }
+      });
+      console.log(`${color}:alive: ${this.#aliveTanks[color]}`);
+
       const probableWinner = this.whoWon();
       if (probableWinner) {
         this.#winner = probableWinner;
-        this.showWinner()
-      }
+        this.showWinner();
+      } else this.changePlayer();
     }
     if (this.#mode === GameMode.arrange) {
       this.addTank(address, new Tank(color));
     }
   };
+
+  /**
+   * Renders HTML structure for grid, and sets the styles...
+   * @param id {string} `id` for the grid element
+   * @param gridState {*} state object of a player
+   */
   private renderGrid(id: string, gridState: any): void {
     let finalGrid = `<div class="grid" id="${id}">\n`;
 
@@ -67,49 +100,84 @@ export default class Game {
     finalGrid += `</div>`;
     this.#parent.innerHTML += finalGrid;
 
-    $(`.grid`).forEach((grid) => {
+    $(`.grid`).forEach((grid: HTMLElement) => {
       grid.style.gridTemplateColumns = `repeat(${this.#gridSize}, 1fr)`;
       grid.style.gridTemplateRows = `repeat(${this.#gridSize}, 1fr)`;
     });
   }
+
+  /**
+   * Figures out that who won. If no one has won, returns `null`
+   * @returns {?Player}
+   */
   private whoWon(): Player | null {
     if (this.#aliveTanks.red === 0) return Player.blue;
     else if (this.#aliveTanks.blue === 0) return Player.red;
     else return null;
   }
+
+  /**
+   * Shows winner with a toast, also `freeze()` the game
+   */
   private showWinner(): void {
     if (this.#winner === Player.blue) {
-      new Toast(Player.blue, 'آبی برنده شد!').show()
+      new Toast(Player.blue, `آبی برنده شد!`).show();
     }
     if (this.#winner === Player.red) {
-      new Toast(Player.red, 'قرمز برنده شد!').show()
+      new Toast(Player.red, `قرمز برنده شد!`).show();
     }
-    this.freeze()
+    this.freeze();
   }
 
+  /**
+   * Disables all buttons except the reload key
+   * to prevent players from playing, and the
+   * grid too.
+   */
   public freeze() {
-    $('.btn:not(#btn-reload)').forEach(btn => btn.disabled = true);
+    $(`.btn:not(#btn-reload)`).forEach((btn) => (btn.disabled = true));
+    $(`.grid`).forEach((grid) =>
+      grid.removeEventListener(`click`, this.handleGridClick)
+    );
   }
 
+  /**
+   * Adds a tank to the grid and to the `state`
+   * @param cellAddress cell address in format of /[A-Z]+\d+/
+   * @param tank a Tank class instance
+   */
   public addTank(cellAddress: string, tank: Tank) {
-    if (tank.color === Player.red) this.#aliveTanks.red++;
-    if (tank.color === Player.blue) this.#aliveTanks.blue++;
+    const alives: any = { blue: 0, red: 0 };
+    [`blue`, `red`].forEach((color) => {
+      for (const cellAddr in this.#state[color]) {
+        const cell = this.#state[color][cellAddr];
+        if (cell instanceof Tank && !cell.isDead) {
+          alives[color]++;
+        }
+        this.#aliveTanks[color] = alives[color]
+      }
+    });
+
     this.#state[tank.color][cellAddress] = tank;
     $(`#${tank.color}`)[0]
       .querySelector(`[data-address=${cellAddress}]`)
       .append(tank.cell);
   }
+
+  /**
+   * Toggles player, color indicator and rotates the grid
+   */
   public changePlayer = () => {
     this.#player = this.#player === Player.blue ? Player.red : Player.blue;
-    this.#parent.classList.toggle("blue");
+    this.#parent.classList.toggle(`blue`);
     if (this.#player === Player.red) {
       document.querySelector(
-        ".color-indicator"
+        `.color-indicator`
       )!.className = `color-indicator ${Player.red}`;
     }
     if (this.#player === Player.blue) {
       document.querySelector(
-        ".color-indicator"
+        `.color-indicator`
       )!.className = `color-indicator ${Player.blue}`;
     }
   };
